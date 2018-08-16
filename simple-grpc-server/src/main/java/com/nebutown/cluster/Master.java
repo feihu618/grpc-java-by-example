fcef305da30581f9fc95b1dcca7c3e9ee0f1a851
ginkgo <yaoxiaoming@jk.cn>
2018-08-16 17:57:12 +0800

    demo for ZKClient

--- simple-grpc-server/src/main/java/com/nebutown/cluster/Master.java
+++ simple-grpc-server/src/main/java/com/nebutown/cluster/Master.java
@@ -2,13 +2,51 @@ package com.nebutown.cluster;
 
 public class Master {
     private DukerZKClient zkClient;
+    private Node.NodeInfo nodeInfo;
 
     public void startUp() {
 
+        zkClient.registerSessionStateChangeHandler(new ZKClient.SessionStateChangeHandler() {
+            @Override
+            public String getName() {
+                return "Master";
+            }
+
+            @Override
+            public void beforeInitializingSession() {
+                //TODO: clean
+            }
+
+            @Override
+            public void afterInitializingSession() {
+                zkClient.registerBroker(nodeInfo);
+
+            }
+        });
     }
 
     public void shutdown() {
         throw new UnsupportedOperationException();
     }
+
+    public void elect() {
+
+    }
+
+    public void reElect() {
+
+    }
+
+    void onElectSuccess() {
+
+    }
+
+    void onElectFail() {
+
+    }
+
+
+
+
 
 }
